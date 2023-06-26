161a30613cac05865fabd5f03448f356a62e6e1c
Hasan Malik <hmalik0412@gmail.com>
2023-06-26 21:20:24 +0100

    Created variables to "city" element/retrieve value

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function getweather() {
     const apiKey = '3ef3feb61b9b45fe96e200638232606';
+
+    const cityInput = document.getElementById('city');
+    const city = cityInput.value;
+  
+    const apiUrl = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`; 
 };
