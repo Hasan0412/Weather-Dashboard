09964fa8d8870b932a5f53e2d878efd64b288fb5
Hasan Malik <hmalik0412@gmail.com>
2023-06-26 21:43:25 +0100

    Added erro message + Weather data on request

--- script.js
+++ script.js
@@ -10,5 +10,25 @@ function getweather() {
     .then(response => response.json())
     .then(data => {
       const weatherCard = document.getElementById('weatherCard');
-});
+
+      if (data.error) {
+        weatherCard.innerHTML = `<p>${data.error.message}</p>`;
+      } else {
+        const { name, country, temp_c, condition, humidity, wind_kph } = data.current;
+        
+        const weatherHTML = `
+          <h2>${name}, ${country}</h2>
+          <p><strong>Temperature:</strong> ${temp_c}°C</p>
+          <p><strong>Condition:</strong> ${condition.text}</p>
+          <p><strong>Humidity:</strong> ${humidity}%</p>
+          <p><strong>Wind Speed:</strong> ${wind_kph} km/h</p>
+        `;
+        
+        weatherCard.innerHTML = weatherHTML;
+      }
+    })
+    .catch(error => {
+      const weatherCard = document.getElementById('weatherCard');
+      weatherCard.innerHTML = '<p>An error occurred while fetching the weather data.</p>';
+    });
 }
