3e75c93c07d771839ed82d9f0b5f83aa6d30468c
Hasan Malik <hmalik0412@gmail.com>
2023-06-29 00:59:32 +0100

    combine current weather and 5 day forecast js

--- script.js
+++ script.js
@@ -4,40 +4,59 @@ function getWeather() {
   const cityInput = document.getElementById('city');
   const city = cityInput.value;
 
-  const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=5`;
+  const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=6`;
 
   fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
+  .then(response => response.json())
+  .then(data => {
       const weatherCard = document.getElementById('weatherCard');
 
       if (data.error) {
-        weatherCard.innerHTML = `<p>${data.error.message}</p>`;
+          weatherCard.innerHTML = `<p>${data.error.message}</p>`;
       } else {
-        const forecast = data.forecast.forecastday;
+          const current = data.current;
+          const forecast = data.forecast.forecastday.slice(1); // Exclude current day
 
-        let weatherHTML = '<h2>5-Day Forecast</h2>';
-
-        forecast.forEach(day => {
-          const date = day.date;
-          const { avgtemp_c, condition, avghumidity, maxwind_kph } = day.day;
+          let weatherHTML = '<h2>Current Weather</h2>';
 
           weatherHTML += `
-            <div class="forecast-card">
-              <h3>${date}</h3>
-              <p><strong>Average Temperature:</strong> ${avgtemp_c}°C</p>
-              <p><strong>Condition:</strong> ${condition.text}</p>
-              <p><strong>Average Humidity:</strong> ${avghumidity}%</p>
-              <p><strong>Max Wind Speed:</strong> ${maxwind_kph} km/h</p>
-            </div>
-`         ;    
-        });
+              <div class="forecast-card">
+                  <h3>${data.location.name}, ${data.location.country}</h3>
+                  <p><strong>Temperature:</strong> ${current.temp_c}°C</p>
+                  <p><strong>Condition:</strong> ${current.condition.text}</p>
+                  <p><strong>Humidity:</strong> ${current.humidity}%</p>
+                  <p><strong>Wind Speed:</strong> ${current.wind_kph} km/h</p>
+              </div>
+          `;
 
-        weatherCard.innerHTML = weatherHTML;
+          weatherHTML += '<h2>5-Day Forecast</h2>';
+
+          forecast.forEach(day => {
+              const date = day.date;
+              const { avgtemp_c, condition, avghumidity, maxwind_kph } = day.day;
+
+              weatherHTML += `
+                  <div class="forecast-card">
+                      <h3>${date}</h3>
+                      <p><strong>Average Temperature:</strong> ${avgtemp_c}°C</p>
+                      <p><strong>Condition:</strong> ${condition.text}</p>
+                      <p><strong>Average Humidity:</strong> ${avghumidity}%</p>
+                      <p><strong>Max Wind Speed:</strong> ${maxwind_kph} km/h</p>
+                  </div>
+              `;
+          });
+
+          weatherCard.innerHTML = weatherHTML;
       }
-    })
-    .catch(error => {
+  })
+  .catch(error => {
       const weatherCard = document.getElementById('weatherCard');
       weatherCard.innerHTML = '<p>An error occurred while fetching the weather data.</p>';
-    });
+  });
 }
+
+
+
+
+
+
